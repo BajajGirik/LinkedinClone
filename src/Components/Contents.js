4d75268f4c131ae77926738e361a4aa9f1585caa
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-12 01:50:20 +0530

    Started with content bar

---
+++ src/Components/Contents.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+function Contents() {
+    return (
+        <ContentsContainer>
+            <ContentInput>
+
+            </ContentInput>
+
+            {/* Posts */}
+        </ContentsContainer>
+    )
+}
+
+export default Contents
+
+const ContentsContainer = styled.div`
+    flex: 0.6;
+`;
+
+const ContentInput = styled.div`
+    background-color: white;
+`;
