2113c5e82c3a2c48723f11aa31504e1518c4a0c2
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-12 12:23:08 +0530

    Styled input for adding a post

--- src/Components/Contents.js
+++ src/Components/Contents.js
@@ -1,10 +1,12 @@
+import { Avatar } from "@material-ui/core";
 import styled from "styled-components"
 
 function Contents() {
     return (
         <ContentsContainer>
             <ContentInput>
-
+                <Avatar />
+                <input type="text" placeholder='Start a post'/>
             </ContentInput>
 
             {/* Posts */}
@@ -20,4 +22,17 @@ const ContentsContainer = styled.div`
 
 const ContentInput = styled.div`
     background-color: white;
+    margin-left: 0.5rem;
+    padding: 1rem;
+    display: flex;
+
+    > input {
+        outline: none;
+        font-size: 0.9rem;
+        border: solid 1px lightgray;
+        border-radius: 30px;
+        flex-grow: 1;
+        margin-left: 1rem;
+        padding: 0.4rem 1rem;  
+    }
 `;
