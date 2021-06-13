2ee183d5e4dfb196e857da55e660e855c41a5e08
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-13 15:43:17 +0530

    Styling posts

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -1,4 +1,8 @@
 import { Avatar } from "@material-ui/core";
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import CommentOutlinedIcon from '@material-ui/icons/CommentOutlined';
+import MobileScreenShareIcon from '@material-ui/icons/MobileScreenShare';
+import SendIcon from '@material-ui/icons/Send';
 import styled from "styled-components"
 
 function Posts({ userimg, name, desc, timestamp, post }) {
@@ -16,6 +20,25 @@ function Posts({ userimg, name, desc, timestamp, post }) {
             <div>
                 { post }
             </div>
+
+            <SocialActions>
+                <div>
+                    <ThumbUpAltOutlinedIcon />
+                    <p>Like</p>
+                </div>
+                <div>
+                    <CommentOutlinedIcon />
+                    <p>Comment</p>
+                </div>
+                <div>
+                    <MobileScreenShareIcon />
+                    <p>Share</p>
+                </div>
+                <div>
+                    <SendIcon />
+                    <p>Send</p>
+                </div>
+            </SocialActions>
         </PostContainer>
     )
 }
@@ -23,6 +46,28 @@ function Posts({ userimg, name, desc, timestamp, post }) {
 export default Posts
 
 const PostContainer = styled.div``;
+
 const UserInfo = styled.div`
     display: flex;
+    margin-bottom: 1rem;
+
+    > .MuiAvatar-root {
+        width: 3rem;
+        height: 3rem;
+        margin-right: 0.5rem;
+    }
+
+    > div {
+        font-size: 0.8rem;
+    }
+
+    > div > :last-child {
+        color: gray;
+        font-size: 0.6rem;
+    }
+`;
+
+const SocialActions = styled.div`
+    display: flex;
+    justify-content: space-around;
 `;
