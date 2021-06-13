491ac70f638fd930b7ffd49caba53a5e9a21fb21
Girik Bajaj <bajajgirik2010@gmail.com>
2021-06-13 21:39:14 +0530

    styled news section and added urls

--- src/Components/SidebarR.js
+++ src/Components/SidebarR.js
@@ -17,9 +17,10 @@ function SidebarR() {
 
     return (
         <SidebarRContainer className='shadow'>
+            <h1>News</h1>
             {trendings.map(trend => (
                 <div>
-                    <h3>{trend.title}</h3>
+                    <a href={trend.url}>{trend.title}</a>
                     <p>{trend.description}</p>
                </div>
             ))}
@@ -32,7 +33,27 @@ export default SidebarR
 const SidebarRContainer = styled.div`
     flex: 0.2;
     padding: 1rem;
+    font-size: 0.7rem;
+    
+    > h1 {
+        text-align: center;
+        margin-bottom: 1rem;
+        font-style: italic;
+        text-decoration: underline;
+    }
+
     > div {
-        margin-bottom: 0.5rem;
+        margin-bottom: 1rem;
+
+        > a {
+            color: black;
+            font-size: 0.88rem;
+            font-weight: 700;
+            text-decoration: none;
+        }
+
+        :hover {
+            background-color: lightgray;
+        }
     }
 `;
